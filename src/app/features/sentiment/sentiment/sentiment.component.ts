34a55969d63bff96922f9e8ce984d18f7aba08c3
Dairan De Jesús <dairandemora.work@gmail.com>
2025-04-26 12:15:54 -0400

    Navbar creado.

---
+++ src/app/features/sentiment/sentiment/sentiment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sentiment',
+  imports: [],
+  templateUrl: './sentiment.component.html',
+  styleUrl: './sentiment.component.css'
+})
+export class SentimentComponent {
+
+}
